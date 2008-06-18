ed5952bb5bb966d2780c3b1e470629b0b7aa678e
Alberto Garcia <agarcia@igalia.com>
2008-06-18 15:42:03 +0200

    Make the treeview in the bookmark window reorderable

--- src/vgl-bookmark-window.c
+++ src/vgl-bookmark-window.c
@@ -283,6 +283,7 @@ vgl_bookmark_window_init (VglBookmarkWindow *self)
         g_object_unref(store);
         gtk_tree_view_set_headers_visible(priv->treeview, FALSE);
         gtk_tree_view_append_column(priv->treeview, col);
+        gtk_tree_view_set_reorderable(priv->treeview, TRUE);
 
         gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrwindow),
                                        GTK_POLICY_AUTOMATIC,
