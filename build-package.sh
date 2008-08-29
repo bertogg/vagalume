431c8dbc244d7ba21d6d933831340ad6f64111cf
Alberto Garcia <agarcia@igalia.com>
2008-08-30 01:32:53 +0200

    2008-08-30 Alberto Garcia <agarcia@igalia.com>
    	* build-package.sh
    	* debian/control.maemo2
    	* debian/control.maemo3
    	* debian/control.maemo4: Different control files for each Maemo
    	version.

--- build-package.sh
+++ build-package.sh
@@ -3,11 +3,23 @@
 set -e
 
 if pkg-config --exists libosso; then # Compile for Maemo
+    libossovers=`pkg-config libosso --modversion | cut -d . -f 1`
+    if ! pkg-config --exists conic; then
+        MAEMOVERS=2
+    elif test "$libossovers" = "1"; then
+        MAEMOVERS=3
+    elif test "$libossovers" = "2"; then
+        MAEMOVERS=4
+    else
+        echo "Unknown maemo version"
+        exit 1
+    fi
+
     if [ ! -f debian/control.debian ]; then
        # Make a backup
         cp -f debian/control debian/control.debian
     fi
-    cp -f debian/control.maemo debian/control
+    cp -f debian/control.maemo$MAEMOVERS debian/control
 fi
 
 dpkg-buildpackage -rfakeroot -b -uc
