a40ecb93639ee5adef67a01a52c692b644f7e86a
Alberto Garcia <agarcia@igalia.com>
2009-08-11 18:44:49 +0200

    autogen.sh: run ./configure unless the NOCONFIGURE variable is set

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-set -e
+set -ex
+
 libtoolize --automake --copy --force
 aclocal
 autoconf --force
@@ -8,3 +9,9 @@ autoheader --force
 automake --add-missing --copy --force-missing --foreign
 glib-gettextize --force --copy
 intltoolize --copy --force --automake
+
+if test x$NOCONFIGURE = x; then
+    ./configure "$@"
+else
+    echo Skipping configure process.
+fi
