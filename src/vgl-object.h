b191383d4a604f5e8fb4e6135b1bc2ff866efa43
Alberto Garcia <agarcia@igalia.com>
2009-09-05 03:54:12 +0200

    VglObject, a very minimalistic implementation of ref-counted objects

---
+++ src/vgl-object.h
@@ -0,0 +1,42 @@
+/*
+ * vgl-object.h -- Very minimalistic implementation of ref-counted objects
+ *
+ * Copyright (C) 2009 Igalia, S.L.
+ * Authors: Alberto Garcia <agarcia@igalia.com>
+ *
+ * This file is part of Vagalume and is published under the GNU GPLv3
+ * See the README file for more details.
+ */
+
+#ifndef VGL_OBJECT_H
+#define VGL_OBJECT_H
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+typedef struct {
+        int header;
+        int refcount;
+        int objsize;
+        GDestroyNotify destroy_func;
+} VglObject;
+
+gpointer
+vgl_object_new_with_size                (int            objsize,
+                                         GDestroyNotify destroy_func);
+
+gpointer
+vgl_object_ref                          (gpointer ptr);
+
+void
+vgl_object_unref                        (gpointer ptr);
+
+
+#define vgl_object_new(type,func) \
+((type *) \
+vgl_object_new_with_size                (sizeof (type), func))
+
+G_END_DECLS
+
+#endif /* VGL_OBJECT_H */
